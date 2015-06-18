c60baaa45d94e8847d7159acfaeb896ff2e11284
Takanori Matsumoto <matscube@gmail.com>
2015-06-18 23:47:52 +0900

    Add field scene

---
+++ Classes/FieldScene.cpp
@@ -0,0 +1,9 @@
+//
+//  FieldScene.cpp
+//  TRAX-MobileApp
+//
+//  Created by TakanoriMatsumoto on 2015/06/18.
+//
+//
+
+#include "FieldScene.h"
---
+++ Classes/FieldScene.h
@@ -0,0 +1,14 @@
+//
+//  FieldScene.h
+//  TRAX-MobileApp
+//
+//  Created by TakanoriMatsumoto on 2015/06/18.
+//
+//
+
+#ifndef __TRAX_MobileApp__FieldScene__
+#define __TRAX_MobileApp__FieldScene__
+
+#include <stdio.h>
+
+#endif /* defined(__TRAX_MobileApp__FieldScene__) */
