8fee664194238d3a4a1a1a050796faefdc607931
Takanori Matsumoto <matscube@gmail.com>
2015-06-20 21:00:56 +0900

    Add template to FieldScene.cpp

--- Classes/FieldScene.cpp
+++ Classes/FieldScene.cpp
@@ -7,3 +7,43 @@
 //
 
 #include "FieldScene.h"
+
+USING_NS_CC;
+
+Scene* FieldScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = FieldScene::create();
+    
+    // add layer as a child to scene
+    scene->addChild(layer);
+    
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool FieldScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+
+void FieldScene::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+    
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
