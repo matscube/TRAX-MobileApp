f7e8fe9650be64d85c091570fb2723d8cf846bf2
Takanori Matsumoto <matscube@gmail.com>
2015-07-12 20:55:38 +0900

    Add Field class

---
+++ Classes/Field.cpp
@@ -0,0 +1,9 @@
+//
+//  Field.cpp
+//  TRAX-MobileApp
+//
+//  Created by TakanoriMatsumoto on 2015/07/12.
+//
+//
+
+#include "Field.h"
---
+++ Classes/Field.h
@@ -0,0 +1,14 @@
+//
+//  Field.h
+//  TRAX-MobileApp
+//
+//  Created by TakanoriMatsumoto on 2015/07/12.
+//
+//
+
+#ifndef __TRAX_MobileApp__Field__
+#define __TRAX_MobileApp__Field__
+
+#include <stdio.h>
+
+#endif /* defined(__TRAX_MobileApp__Field__) */
