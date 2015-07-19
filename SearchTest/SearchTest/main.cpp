43567a9eaff1808e791eb406c12b3095a1f5a195
Takanori Matsumoto <matscube@gmail.com>
2015-07-19 23:22:58 +0900

    Add project for search algorithm test

---
+++ SearchTest/SearchTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SearchTest
+//
+//  Created by TakanoriMatsumoto on 2015/07/19.
+//  Copyright (c) 2015年 TakanoriMatsumoto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
