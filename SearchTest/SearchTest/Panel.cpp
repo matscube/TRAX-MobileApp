f4750fbf0a65df9cf3f30ff35fda93f78254c4a2
Takanori Matsumoto <matscube@gmail.com>
2015-07-20 22:08:02 +0900

    Update search test

---
+++ SearchTest/SearchTest/Panel.cpp
@@ -0,0 +1,9 @@
+//
+//  Panel.cpp
+//  SearchTest
+//
+//  Created by TakanoriMatsumoto on 2015/07/20.
+//  Copyright (c) 2015年 TakanoriMatsumoto. All rights reserved.
+//
+
+#include "Panel.h"
---
+++ SearchTest/SearchTest/Panel.h
@@ -0,0 +1,14 @@
+//
+//  Panel.h
+//  SearchTest
+//
+//  Created by TakanoriMatsumoto on 2015/07/20.
+//  Copyright (c) 2015年 TakanoriMatsumoto. All rights reserved.
+//
+
+#ifndef __SearchTest__Panel__
+#define __SearchTest__Panel__
+
+#include <stdio.h>
+
+#endif /* defined(__SearchTest__Panel__) */
