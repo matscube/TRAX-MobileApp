60a5df46a34cc23a88ae475920343a6c31c10c8e
Takanori Matsumoto <matscube@gmail.com>
2015-07-24 22:38:05 +0900

    SearchTest: add class

---
+++ SearchTest/SearchTest/Tester.cpp
@@ -0,0 +1,9 @@
+//
+//  Tester.cpp
+//  SearchTest
+//
+//  Created by TakanoriMatsumoto on 2015/07/24.
+//  Copyright (c) 2015年 TakanoriMatsumoto. All rights reserved.
+//
+
+#include "Tester.h"
---
+++ SearchTest/SearchTest/Tester.h
@@ -0,0 +1,14 @@
+//
+//  Tester.h
+//  SearchTest
+//
+//  Created by TakanoriMatsumoto on 2015/07/24.
+//  Copyright (c) 2015年 TakanoriMatsumoto. All rights reserved.
+//
+
+#ifndef __SearchTest__Tester__
+#define __SearchTest__Tester__
+
+#include <stdio.h>
+
+#endif /* defined(__SearchTest__Tester__) */
